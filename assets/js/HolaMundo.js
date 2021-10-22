c294001f589901af05c4fe253e257e3ef60f81ec
Salvador <slb_18@hotmail.com>
2021-10-22 17:22:23 -0500

    Commit1

---
+++ assets/js/HolaMundo.js
@@ -0,0 +1,11 @@
+//VARIABLES
+var nombre = "Salvador";
+var edad = 37;
+var nuevaEdad = 1;
+
+console.log("Hola: " + nombre + " tienes "+ edad + " años.");
+
+alert(edad + nuevaEdad);
+
+// clg ENTER hace console.log
+console.log(object);
---
+++ assets/js/alerts.js
@@ -0,0 +1,8 @@
+
+let Nombre;
+let edad;
+
+Nombre = prompt("Introduzca su Nombre: " );
+edad = prompt("Introduzca su Edad: " );
+
+alert("Hola: " + Nombre + " su edad es: "+ edad);
